62e23e419bdc2b9853e767d696b5b79c36a193dd
Mugur-Cristian Iacob <mugur-cristian.iacob@kcl.ac.uk>
2021-12-20 16:30:47 +0200

    Implement syard function

--- core3/postfix.scala
+++ core3/postfix.scala
@@ -16,6 +16,16 @@ val precs = Map("+" -> 1,
 		"*" -> 2,
 		"/" -> 2)
 
+val parentheses = Map(")" -> "(",
+		"]" -> "[",
+		"}" -> "{")
+
+// the left parentheses used in the algorithm
+val left_parentheses = List("(","[","{")
+
+// the right parentheses used in the algorithm
+val right_parentheses = List(")","]","}")
+
 // helper function for splitting strings into tokens
 def split(s: String) : Toks = s.split(" ").toList
 
@@ -37,22 +47,53 @@ def split(s: String) : Toks = s.split(" ").toList
 // it helpful to implement two auxiliary functions for the pattern matching:  
 // 
 
-def is_op(op: String) : Boolean = ???
-def prec(op1: String, op2: String) : Boolean = ???
+def is_op(op: String) : Boolean = ops.contains(op)
+	
+
+def prec(op1: String, op2: String) : Boolean = precs.get(op1).get > precs.get(op2).getOrElse(0)
 
 
-def syard(toks: Toks, st: Toks = Nil, out: Toks = Nil) : Toks = ???
+def isAllDigits(x: String) = x forall Character.isDigit 
+
+def get_head(st: Toks) : String = st match {
+
+	case Nil => ""
+	case element :: rest => element
+}
+
+def pop_elements(st: Toks,left_parentheses : String) : Toks = st match {
+
+	case Nil => Nil
+	case element :: rest if (left_parentheses == element) => rest 
+	case element :: rest => pop_elements(rest,left_parentheses) 
+}
+
+def get_popped_elements(st: Toks,left_parentheses : String) : Toks = st.dropRight(st.length - st.indexOf(left_parentheses))
+
+
+def syard(toks: Toks, st: Toks = Nil, out: Toks = Nil) : Toks = toks match {
+    
+	case Nil => out ::: st 
+	case element :: rest if isAllDigits(element) => syard(rest,st,out ::: element :: Nil)
+	case element :: rest if (is_op(element) && prec(element,get_head(st)) )  => syard(rest,element :: st, out)
+	case element :: rest if (is_op(element) && prec(element,get_head(st) ) == false ) => syard(rest,element :: Nil,out ::: st)
+	case element :: rest if (left_parentheses.contains(element)) => syard(rest, element :: st , out)
+	case element :: rest if (right_parentheses.contains(element)) => syard(rest, pop_elements(st,parentheses.get(element).get),out ::: get_popped_elements(st,parentheses.get(element).get)  )
+	  
+}
+
+ 
 
 
 // test cases
-//syard(split("3 + 4 * ( 2 - 1 )"))  // 3 4 2 1 - * +
-//syard(split("10 + 12 * 33"))       // 10 12 33 * +
-//syard(split("( 5 + 7 ) * 2"))      // 5 7 + 2 *
-//syard(split("5 + 7 / 2"))          // 5 7 2 / +
-//syard(split("5 * 7 / 2"))          // 5 7 * 2 /
-//syard(split("9 + 24 / ( 7 - 3 )")) // 9 24 7 3 - / +
+//C3a.syard(C3a.split("3 + 4 * ( 2 - 1 )"))  // 3 4 2 1 - * +
+//C3a.syard(C3a.split("10 + 12 * 33"))       // 10 12 33 * +
+//C3a.syard(C3a.split("( 5 + 7 ) * 2"))      // 5 7 + 2 *
+//C3a.syard(C3a.split("5 + 7 / 2"))          // 5 7 2 / +
+//C3a.syard(C3a.split("5 * 7 / 2"))          // 5 7 * 2 /
+//C3a.syard(C3a.split("9 + 24 / ( 7 - 3 )")) // 9 24 7 3 - / +
 
-//syard(split("3 + 4 + 5"))           // 3 4 + 5 +
+//C3a.syard(C3a.split("3 + 4 + 5"))           // 3 4 + 5 +
 //syard(split("( ( 3 + 4 ) + 5 )"))    // 3 4 + 5 +
 //syard(split("( 3 + ( 4 + 5 ) )"))    // 3 4 5 + +
 //syard(split("( ( ( 3 ) ) + ( ( 4 + ( 5 ) ) ) )")) // 3 4 5 + +
